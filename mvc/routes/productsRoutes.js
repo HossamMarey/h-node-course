ecd042b449bc3a8af4d42a7e615684ba1d629184
HossamMarey <hmarey@ymail.com>
2024-09-01 16:49:35 +0300

    start mvc

--- mvc/routes/productsRoutes.js
+++ mvc/routes/productsRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const fs = require("fs/promises");
 const rootDir = require("../utils/path");
 
 
 router.get("/", (req, res) => {
+
   res.sendFile(path.join(rootDir, "views", "products.html"));
 });
 
